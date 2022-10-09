3476cb64ba08d0606a42f51bb27ea877899c3bc0
Khvorova_Liza <liza.khvorova@gmail.com>
2022-10-09 15:46:36 +0300

    added cancel click in doneBox

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ let todos = [];
 let doneTodos = [];
 
 btn.addEventListener('click', () => {
-
   if (newTask.value.length === 0) {
     alert('Please, enter your task');
   } else {
@@ -18,18 +17,16 @@ btn.addEventListener('click', () => {
       value: newTask.value
     });
     const newNodes = todos.map((item, index) =>
-      ` <li data-id="${index}"> ${item.value} 
-    <div class="act-container">
+      ` <li data-id="${index}"> <div>${item.value}</div>
+      <div class="act-container">
       <button data-check-id="${index}" class="check" src="check.svg" alt="check"></button>
       <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
-    </div>
-  </li>
-  `).join('');
+      </div>
+      </li>
+      `).join('');
     taskBox.innerHTML = newNodes;
     newTask.value = '';
-
   }
-
 });
 
 taskBox.addEventListener('click', (event) => {
@@ -42,25 +39,24 @@ taskBox.addEventListener('click', (event) => {
 
     const newNodes = todos.map((item, index) =>
       ` <li data-id="${index}"> ${item.value} 
-    <div class="act-container">
+      <div class="act-container">
       <button data-check-id="${index}" class="check" src="check.svg" alt="check"></button>
       <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
-    </div>
-  </li>
-  `).join('');
+      </div>
+      </li>
+      `).join('');
     taskBox.innerHTML = newNodes;
 
     const doneNodes = doneTodos.map((item, index) =>
-      ` <li data-id="${index}"> ${item.value} 
-  <div class="act-container">
-    <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
-  </div>
-</li>
-`).join('');
+      `<li data-id="${index}"> ${item.value} 
+      <div class="act-container">
+      <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
+      </div>
+      </li>
+      `).join('');
     doneBox.innerHTML = doneNodes;
   }
 
-  console.log(event);
   if (event.target.dataset.cancelId !== undefined) {
     const index = event.target.dataset.cancelId;
     todos = todos.filter((_, i) => {
@@ -68,53 +64,32 @@ taskBox.addEventListener('click', (event) => {
     });
 
     const newNodes = todos.map((item, index) =>
-      ` <li data-id="${index}"> ${item.value} 
-    <div class="act-container">
+      `<li data-id="${index}"> ${item.value} 
+      <div class="act-container">
       <button data-check-id="${index}" class="check" src="check.svg" alt="check"></button>
       <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
-    </div>
-  </li>
-  `).join('');
+      </div>
+      </li>
+      `).join('');
     taskBox.innerHTML = newNodes;
+  }
+});
+
+doneBox.addEventListener('click', (event) => {
+  if (event.target.dataset.cancelId !== undefined) {
+    const index = event.target.dataset.cancelId;
+    doneTodos = doneTodos.filter((_, i) => {
+      return i !== +index;
+    });
+
+    const Nodes = doneTodos.map((item, index) =>
+      `<li data-id="${index}"> ${item.value} 
+      <div class="act-container">
+      <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
+      </div>
+      </li>
+      `).join('');
+    doneBox.innerHTML = Nodes;
   }
 
 });
-
-
-
-checkBtns.forEach(element => {
-  console.log(element);
-  element.addEventListener('click', (event) => {
-
-
-    //   const doneNodes = `
-    //      <li data-id="${index}"> ${item.value} 
-    //   <div class="act-container">
-    //     <button data-cancel-id="${index}" class="cancel" src="cancel.svg" alt="cancel"></button>
-    //   </div>
-    // </li>
-    //      `;
-    // doneBox.innerHTML = doneNodes;
-    // todos.splice(index, 1);
-
-  });
-});
-
-//     cancel.forEach(element => {
-//       element.addEventListener('click', () => {
-//         newNode.;
-//       });
-//     });
-// });
-
-
-
-
-
-
-// input.addEventListener('keypress', (keyPressed) => {
-//   const keyEnter = '';
-//   if (keyPressed.which == keyEnter) {
-//     createNewTask();
-//   }
-// });
