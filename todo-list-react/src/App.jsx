0360d4e91f6ba740b40e96e4fca1d626077147b7
Khvorova_Liza <liza.khvorova@gmail.com>
2023-04-13 18:04:11 +0300

    layout center

--- todo-list-react/src/App.jsx
+++ todo-list-react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Input } from './components/Input/Input';
 import { TaskBox } from './components/TaskBox/TaskBox';
@@ -7,13 +7,13 @@ import { DeletedTodoBox } from './components/DeletedTodoBox/DeletedTodoBox';
 import { Tabs } from './components/Tabs/Tabs';
 
 function App() {
-  const [todo, setTodo] = useState([{id: 1, value: "Hello"}, {id: 2, value: "Hey"}]);
+  const [todo, setTodo] = useState([{ id: 1, value: "Hello" }, { id: 2, value: "Hey" }]);
   const [doneTodo, setDoneTodo] = useState([]);
   const [deletedTodo, setDeletedTodo] = useState([]);
   const [activeTab, setActiveTab] = useState("active");
 
   function handleAddTodo(value) {
-    setTodo([...todo, {id: Date.now(), value}])
+    setTodo([...todo, { id: Date.now(), value }])
   }
 
   function handleRemoveTodo(id) {
@@ -42,49 +42,51 @@ function App() {
     setDeletedTodo([...deletedTodo, removedTodoFromDone]);
     handleRemoveDoneTodo(id);
   }
- 
+
   function handleSetTab(id) {
     setActiveTab(id);
   }
 
   return (
-    <>
-      <h1>To Do list</h1>
-      <div className="widget-container">
-        {activeTab === 'active' ? 
-          <div className="widget">
-            <Input
-              addTodo={handleAddTodo}
-            />
-            <TaskBox
-              todo={todo}
-              removeTodo={handleAddDeletedFromTaskBox}
-              checkTodo={handleAddDoneTodo}
-            />
-          </div> : 
-        activeTab === 'done' ? 
-          <div className="widget">
-            <WellDoneBox
-              doneTodo={doneTodo}
-              removeTodo={handleAddDeletedFromDoneBox}
-            
-            />
-          </div> :  
-          <div className="widget">
-            <DeletedTodoBox
-              deletedTodo={deletedTodo}
-              removeTodo={handleRemoveTodo}
-              checkTodo={handleAddDoneTodo}
-  
-            />
-          </div>
-        }
-        <Tabs
-          activeTab={activeTab}
-          setTab={handleSetTab}
-        />
+    <div className='row-container'>
+      <div className="container">
+        <h1>To Do list</h1>
+        <div className="widget-container">
+          {activeTab === 'active' ?
+            <div className="widget">
+              <Input
+                addTodo={handleAddTodo}
+              />
+              <TaskBox
+                todo={todo}
+                removeTodo={handleAddDeletedFromTaskBox}
+                checkTodo={handleAddDoneTodo}
+              />
+            </div> :
+            activeTab === 'done' ?
+              <div className="widget">
+                <WellDoneBox
+                  doneTodo={doneTodo}
+                  removeTodo={handleAddDeletedFromDoneBox}
+
+                />
+              </div> :
+              <div className="widget">
+                <DeletedTodoBox
+                  deletedTodo={deletedTodo}
+                  removeTodo={handleRemoveTodo}
+                  checkTodo={handleAddDoneTodo}
+
+                />
+              </div>
+          }
+        </div>
       </div>
-    </>
+      <Tabs
+        activeTab={activeTab}
+        setTab={handleSetTab}
+      />
+    </div>
   );
 }
 
